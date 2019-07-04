4b0c603a17e2729ea221a24f75204d5a5124d709
lenovo <312805319@qq.com>
2019-07-04 17:00:05 +0800

    commit TestNG Demo

--- src/test/java/TestDemo.java
+++ src/test/java/TestDemo.java
@@ -11,7 +11,7 @@ public class TestDemo {
     @BeforeTest
     public void init() {
         String url = "http://www.baidu.com";
-        System.setProperty("webdriver.chrome.driver", "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chromedriver.exe");
+        //System.setProperty("webdriver.chrome.driver", "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chromedriver.exe");
         driver = new ChromeDriver();
         driver.get(url);
     }
@@ -21,6 +21,7 @@ public class TestDemo {
         //输入搜索字符串
         driver.findElement(By.id("kw")).sendKeys(searchWord);
         //点击[百度一下]按钮
+        System.out.print(driver.findElement(By.id("su")).getText());
         driver.findElement(By.id("su")).click();
         Thread.sleep(2000);
     }
