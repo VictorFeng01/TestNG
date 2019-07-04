66763af47061d5cc852912b8e7aa7f738e04c580
lenovo <312805319@qq.com>
2019-07-04 13:43:41 +0800

    commit TestNG Demo

---
+++ src/test/java/TestDemo.java
@@ -0,0 +1,31 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+public class TestDemo {
+    WebDriver driver;
+    @BeforeTest
+    public void init() {
+        String url = "http://www.baidu.com";
+        driver = new ChromeDriver();
+        driver.get(url);
+    }
+    @Test
+    @Parameters("Java")
+    public void search(String searchWord) throws InterruptedException {
+        //输入搜索字符串
+        driver.findElement(By.id("kw")).sendKeys(searchWord);
+        //点击[百度一下]按钮
+        driver.findElement(By.id("su")).click();
+        Thread.sleep(2000);
+    }
+
+    @AfterTest
+    public void teardown() {
+        driver.quit();
+    }
+}
