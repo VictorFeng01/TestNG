8fecc75d422d36e23a87bad5ddfb31f70e933d5d
lenovo <312805319@qq.com>
2019-07-05 11:58:06 +0800

    commit TestNG Demo

--- src/test/java/TestDemo.java
+++ src/test/java/TestDemo.java
@@ -6,26 +6,38 @@ import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
 public class TestDemo {
     WebDriver driver;
     @BeforeTest
     public void init() {
-        System.out.print("***************************");
         String url = "http://www.baidu.com";
         System.setProperty("webdriver.chrome.driver", "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chromedriver.exe");
         driver = new ChromeDriver();
         driver.get(url);
     }
-    @Test
-    @Parameters("Java")
-    public void search(String searchWord) throws InterruptedException {
+    @Test(priority = 0)
+    //@Parameters("Java")
+    public void search() throws InterruptedException {
         //输入搜索字符串
-        driver.findElement(By.id("kw")).sendKeys(searchWord);
+        driver.findElement(By.id("kw")).sendKeys("Java");
+        Thread.sleep(5000);
         //点击[百度一下]按钮
-        System.out.print(driver.findElement(By.id("su")).getText());
         driver.findElement(By.id("su")).click();
         Thread.sleep(2000);
     }
+    @Test(priority = 1)
+    //@Parameters("ditu")
+    public void diTu() throws InterruptedException{
+        driver.findElement(By.id("kw")).clear();
+        Thread.sleep(5000);
+        driver.findElement(By.id("kw")).sendKeys("Python");
+        Thread.sleep(3000);
+        driver.findElement(By.id("su")).click();
+        Thread.sleep(2000);
+}
 
     @AfterTest
     public void teardown() {
